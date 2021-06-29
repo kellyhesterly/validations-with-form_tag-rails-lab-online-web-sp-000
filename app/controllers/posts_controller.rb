a1c93289fb97aeff70af2a3bcf7fdc43c6705d8a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-29 15:15:34 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,8 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-      if @post.update(post_params)
+      @post.update(post_params)
+      if @post.save
         redirect_to post_path(@post)
       else
         render :new
